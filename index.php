187c123460b274a35068e4d37c3e61c695a054e9
muax3000 <maximilian.huhle@hotmail.de>
2024-02-29 12:19:31 +0100

    Send email if code is not working anymore

--- index.php
+++ index.php
@@ -37,3 +37,22 @@ if (is_plugin_active('elementor/elementor.php')) {
         add_action('wp_enqueue_scripts', 'host_fontawesome_local_remove_elementor', 20);
     }
 }
+
+// Check if Elementor was updated
+
+if (!function_exists('host_fontawesome_local_check_elementor_update')) {
+    function host_fontawesome_local_check_elementor_update($upgrader_object, $options)
+    {
+        if ($options['action'] === 'update' && $options['type'] === 'plugin' && $options['plugins'][0] === 'elementor/elementor.php') {
+            // Elementor was updated, check if code is still working
+            if (!wp_style_is('fontawesome', 'enqueued')) {
+                // Code is not working, send email notification
+                $to = 'maximilian.huhle@hotmail.de';
+                $subject = 'Host FontAwesome Locally - Code not working anymore';
+                $message = 'The code for hosting FontAwesome locally is not working anymore.';
+                wp_mail($to, $subject, $message);
+            }
+        }
+    }
+    add_action('upgrader_process_complete', 'host_fontawesome_local_check_elementor_update', 10, 2);
+}
